28154e184190015b435dc33ea568bf8c9ccac7d5
maektwain <saranshisat@icloud.com>
2019-03-29 01:48:55 +0530

    Changed Few Things

--- p2pool/networks/dash.py
+++ p2pool/networks/dash.py
@@ -2,10 +2,10 @@ from p2pool.dash import networks
 
 PARENT = networks.nets['dash']
 SHARE_PERIOD = 20 # seconds
-CHAIN_LENGTH = 2*1*1//2 # shares
-REAL_CHAIN_LENGTH = 24*60*60//20 # shares
-TARGET_LOOKBEHIND = 100 # shares  //with that the pools share diff is adjusting faster, important if huge hashing power comes to the pool
-SPREAD = 10 # blocks
+CHAIN_LENGTH = 2*1*1//2  # shares
+REAL_CHAIN_LENGTH = 2*1*1//2 # shares
+TARGET_LOOKBEHIND = 10 # shares  //with that the pools share diff is adjusting faster, important if huge hashing power comes to the pool
+SPREAD = 5 # blocks
 IDENTIFIER = '7242ef345e1bed6b'.decode('hex')
 PREFIX = '3b3e1286f446b891'.decode('hex')
 COINBASEEXT = '0D2F5032506F6F6C2D444153482F'.decode('hex')
